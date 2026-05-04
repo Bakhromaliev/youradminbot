69ba1c27855202ac162ef5ce0356b42d119c4e43
Bakhromaliev <116061709+Bakhromaliev@users.noreply.github.com>
2026-05-04 04:41:28 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -8,6 +8,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
 
+# Render PostgreSQL havolasini asyncpg ga moslash
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
